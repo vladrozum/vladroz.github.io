fb8c40b90f1cab85bc83cbd0cd57c04f385bc0a7
vladrozum <vladrozum777@gmail.com>
2023-04-30 22:34:54 +0300

    5

--- src/choice.js
+++ src/choice.js
@@ -1,1 +1,9 @@
 import React from "react";
+
+export default function Choice(props){
+    return(
+        <div>     
+            <button className="button">{props.value}</button>
+        </div>
+    )
+}
--- src/main.js
+++ src/main.js
@@ -1,83 +1,78 @@
 import React from "react";
 import "./index.css";
 import data from "./data";
+import Choice from "./choice";
 
 export default function Main() {
   const [dataArray, setData] = React.useState(data);
-  const [quest, setQuest] = React.useState(generateQuestions);
+  const [quest, setQuest] = React.useState(generateAllQuestions());
 
   function random() {
     const categories = ["life", "business", "YesNot"];
     const randomNumber = Math.floor(Math.random() * categories.length);
     const chooseCategory = categories[randomNumber];
     const choose = dataArray[randomNumber].answers[chooseCategory];
-    console.log(choose);
+    console.log(choose.split('\n')[0]);
     return choose;
   }
 
   function chooseRandomAnswer() {
-    const arrAnswer = []
+    const arrAnswer = [];
     const categories = ["life", "business", "YesNot"];
     const randomNumber = Math.floor(Math.random() * categories.length);
-    const choose = dataArray[randomNumber].answers[categories[randomNumber]]
+    const choose = dataArray[randomNumber].answers[categories[randomNumber]];
     const url = dataArray[randomNumber].img;
-    arrAnswer.push(choose, url)
-    return arrAnswer
+    arrAnswer.push(choose, url);
+    return arrAnswer;
   }
 
-    function generateQuestions() {
-      const newArray = [];
-      for (let i = 0; i < 3; i++) {
-        const newQuestion = {
-          value: random(),
-          held: false,
-          answer: false,
-          id: i + 1,
-        };
-        newArray.push(newQuestion);
-      }
-      const rightAnswer = {
-        value: chooseRandomAnswer()[0],
+  function generateAllQuestions() {
+    const newArray = [];
+    for (let i = 0; i < 3; i++) {
+      const newQuestion = {
+        value: random(),
         held: false,
-        answer: true,
-        id: 4,
+        answer: false,
+        id: i + 1,
       };
-      newArray.push(rightAnswer);
-      console.log(newArray)
-      return newArray;
-
+      newArray.push(newQuestion);
     }
+    const rightAnswer = {
+      url: chooseRandomAnswer()[1],
+      value: chooseRandomAnswer()[0],
+      held: false,
+      answer: true,
+      id: 4,
+    };
+    newArray.push(rightAnswer);
+    // console.log(newArray);
+    return newArray;
+  }
 
-    
-    return (
-      <div>
-        <div className="container">
-          <img src={quest.randomImage} alt="" className="images"></img>
-        </div>
-        <div className="button-next">
-          <button onClick={chooseRandomAnswer} className="but">
-            Наступне питання
-          </button>
-        </div>
-        <div className="question-container">
-          <button>{quest.first}</button>
-          <button>{quest.second}</button>
-          <button>{quest.third}</button>
-          <button>{quest.fourth}</button>
-        </div>
-      </div>
+  function generateQuestions() {
+    setQuest((oldQuestion) =>
+      oldQuestion.map((die, i) => {
+        return { value: random(), held: false, id: i + 1 };
+      })
     );
   }
+  
+  const quizElements = quest.map((question) => (
+    <Choice key={question.id} {...question} />
+  ));
 
-
-//   setQuest(() => (
-
-//     {
-//     randomImage: url,
-//     first: answ,
-//     second: random(),
-//     third: random(),
-//     fourth: random()
-//   }));
-
-// }
+  return (
+    <div>
+      <div className="container">
+        <p className="whats-true">Що з цього правда?</p>
+        <img src={generateAllQuestions()[3].url} alt="" className="images"></img>
+      </div>
+      <div className="button-next">
+        <button onClick={generateQuestions} className="but">
+          Наступне питання
+        </button>
+      </div>
+      <div className="question-container">{quizElements}</div>
+    </div>
+  );
+}
