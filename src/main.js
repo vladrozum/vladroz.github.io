f9b41706a382217ff6465a1c7824c727fbfca2e0
vladrozum <vladrozum777@gmail.com>
2023-04-30 18:29:39 +0300

    4

--- src/main.js
+++ src/main.js
@@ -1,68 +1,83 @@
 import React from "react";
 import "./index.css";
 import data from "./data";
-import Questions from "./questions";
 
 export default function Main() {
-  
   const [dataArray, setData] = React.useState(data);
-  const [quest, setQuest] = React.useState({
-    randomImage: "https://www.tarotcardmeanings.net/images/tarotcards-large/tarot-magician.jpg",   
-    first: "",
-    second: "",
-    third: "",
-    fourth: ""
-  })
+  const [quest, setQuest] = React.useState(generateQuestions);
 
-  // console.log(dataArray);
-
-  // function chooseRandomUrl() {
-  //   const randomNumber = Math.floor(Math.random() * dataArray.length);
-  //   const url = dataArray[randomNumber].img;
-  //   setUrl((prevUrl) => ({
-  //     ...prevUrl,
-  //     randomImage: url,
-  //   }));
-  // }
-
-  function random(){
-    const categories = ['life', 'business', 'YesNot']
+  function random() {
+    const categories = ["life", "business", "YesNot"];
     const randomNumber = Math.floor(Math.random() * categories.length);
-    const chooseCategory = categories[randomNumber]
-    const choose = dataArray[randomNumber].answers[chooseCategory]
-    return choose
-
+    const chooseCategory = categories[randomNumber];
+    const choose = dataArray[randomNumber].answers[chooseCategory];
+    console.log(choose);
+    return choose;
   }
 
-  function chooseRandomAnswer(){
-    const randomNumberForRight = Math.floor(Math.random() * dataArray.length);
-    const answ = dataArray[randomNumberForRight].answers.YesNot;
-    const url = dataArray[randomNumberForRight].img;
+  function chooseRandomAnswer() {
+    const arrAnswer = []
+    const categories = ["life", "business", "YesNot"];
+    const randomNumber = Math.floor(Math.random() * categories.length);
+    const choose = dataArray[randomNumber].answers[categories[randomNumber]]
+    const url = dataArray[randomNumber].img;
+    arrAnswer.push(choose, url)
+    return arrAnswer
+  }
 
-    setQuest(() => (
-      
-      {
-      randomImage: url,
-      first: random(),
-      second: random(),
-      third: random(),
-      fourth: random()
-    }));
-  
+    function generateQuestions() {
+      const newArray = [];
+      for (let i = 0; i < 3; i++) {
+        const newQuestion = {
+          value: random(),
+          held: false,
+          answer: false,
+          id: i + 1,
+        };
+        newArray.push(newQuestion);
+      }
+      const rightAnswer = {
+        value: chooseRandomAnswer()[0],
+        held: false,
+        answer: true,
+        id: 4,
+      };
+      newArray.push(rightAnswer);
+      console.log(newArray)
+      return newArray;
+
+    }
+
+    
+    return (
+      <div>
+        <div className="container">
+          <img src={quest.randomImage} alt="" className="images"></img>
+        </div>
+        <div className="button-next">
+          <button onClick={chooseRandomAnswer} className="but">
+            Наступне питання
+          </button>
+        </div>
+        <div className="question-container">
+          <button>{quest.first}</button>
+          <button>{quest.second}</button>
+          <button>{quest.third}</button>
+          <button>{quest.fourth}</button>
+        </div>
+      </div>
+    );
   }
 
 
-  return (
-    <div>
-      <div className="container">
-        <img src={quest.randomImage} alt="" className="images"></img>
-      </div>
-      <div className="button-next">
-        <button onClick={chooseRandomAnswer} className="but">
-          Наступне питання
-        </button>
-      </div>
-       <div className="question-container">{}</div> 
-    </div>
-  );
-}
+//   setQuest(() => (
+
+//     {
+//     randomImage: url,
+//     first: answ,
+//     second: random(),
+//     third: random(),
+//     fourth: random()
+//   }));
+
+// }
