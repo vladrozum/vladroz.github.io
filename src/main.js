b0835ef1df50200a524e3ac51b48d404a7c4473d
vladrozum <vladrozum777@gmail.com>
2023-05-02 19:44:41 +0300

    6

--- src/main.js
+++ src/main.js
@@ -1,91 +1,97 @@
 import React from "react";
 import "./index.css";
 import data from "./data";
-import Choice from "./choice";
+
 
 export default function Main() {
   const [dataArray, setData] = React.useState(data);
   const [quest, setQuest] = React.useState(generateAllQuestions());
+  const [set, setRight] = React.useState(false)
+ 
+  // React.useEffect(()=>{
+  //   if (answerQuestion)
+  // }
+  // )
 
-  function random() {
-    const categories = ["life", "business", "YesNot"];
-    const randomNumber = Math.floor(Math.random() * categories.length);
-    const randomNumber2 = Math.floor(Math.random() * dataArray.length);
-    const chooseCategory = categories[randomNumber];
-    const choose = dataArray[randomNumber2].answers[chooseCategory];
-    console.log(choose.split('\n')[0]);
-    return choose;
-  }
-
-  function chooseRandomAnswer() {
-    const arrAnswer = [];
-    const categories = ["life", "business", "YesNot"];
-    const randomNumber = Math.floor(Math.random() * categories.length);
-    const randomNumber2 = Math.floor(Math.random() * dataArray.length);
-    const choose = dataArray[randomNumber2].answers[categories[randomNumber]];
-    const url = dataArray[randomNumber2].img;
-    arrAnswer.push(choose, url);
-    return arrAnswer;
-  }
-
+  
+    function random() {
+      const categories = ["life", "business", "YesNot"];
+      const randomNumber = Math.floor(Math.random() * categories.length);
+      const randomNumber2 = Math.floor(Math.random() * dataArray.length);
+      const chooseCategory = categories[randomNumber];
+      const choose = dataArray[randomNumber2].answers[chooseCategory];
+      return choose;
+    }
+  
+    function chooseRandomAnswer() {
+      const arrAnswer = [];
+      const categories = ["life", "business", "YesNot"];
+      const randomNumber = Math.floor(Math.random() * categories.length);
+      const randomNumber2 = Math.floor(Math.random() * dataArray.length);
+      const choose = dataArray[randomNumber2].answers[categories[randomNumber]];
+      const url = dataArray[randomNumber2].img;
+      arrAnswer.push(choose, url);
+      return arrAnswer;
+    }
   function generateAllQuestions() {
-    const url = chooseRandomAnswer()[1]
-    const val = chooseRandomAnswer()[0]
-    const newArray = [];
+    const url = chooseRandomAnswer()[1];
+    const newArray = {url: url, answerOptions: []};
     for (let i = 0; i < 3; i++) {
       const newQuestion = {
-        url: url,
         value: random(),
-        held: false,
-        answer: false,
-        id: i + 1,
+        isCorrect: false,
       };
-      newArray.push(newQuestion);
+      newArray.answerOptions.push(newQuestion)
     }
+
     const rightAnswer = {
-      url: url,
-      value: val,
-      held: false,
-      answer: true,
-      id: 4,
+      value: chooseRandomAnswer()[0],
+      isCorrect: true,
     };
-    newArray.push(rightAnswer);
-    // console.log(newArray);
-    return newArray;
+   newArray.answerOptions.push(rightAnswer)
+
+
+    return [newArray];
   }
+
+
 
   function generateQuestions() {
-    setQuest(generateAllQuestions())
+    setQuest(generateAllQuestions());
+  }
+
+  function checkAnswer(isCorrect){
+    if(isCorrect){
+      setRight(true)
+    }
+  
 
   }
 
-  
-  function answerQuestion(id) {
-    setQuest((prevQuest) => prevQuest.map(questions => {
-        return questions.id===id? 
-            {...prevQuest, held: true} : 
-            prevQuest
-    }))
-  }
 
-  console.log(quest)
-  
-  const quizElements = quest.map((question) => (
-    <Choice key={question.id} {...question} hold={()=> answerQuestion(question.id)}/>
-  ));
+  const styles = {
+    backgroundColor : set? 'green' : ""
+  } 
 
   return (
     <div>
       <div className="container">
         <p className="whats-true">Що з цього правда?</p>
-        <img src={generateAllQuestions()[0].url} alt="" className="images"></img>
+        <img
+          src={generateAllQuestions()[0].url}
+          alt=""
+          className="images"
+        ></img>
       </div>
       <div className="button-next">
-        <button onClick={generateQuestions} className="but">
+        {set && <button onClick={generateQuestions} className="but">
           Наступне питання
-        </button>   
+        </button>}
       </div>
-      <div className="question-container">{quizElements}</div>
+      <div className="question-container">
+        {quest[0].answerOptions.map((question) => (
+      <button className="button" style={styles} onClick={()=> checkAnswer(question.isCorrect)}>{question.value}</button>))}
+      </div>
     </div>
   );
 }
