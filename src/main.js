c4b90b17cc7a94b9dedf17bbfeda8da6b735a886
vladrozum <vladrozum777@gmail.com>
2023-05-02 21:53:30 +0300

    9

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import "./index.css";
 import data from "./data";
 
 export default function Main() {
+
+
   const [dataArray, setData] = React.useState(data);
   const [quest, setQuest] = React.useState(generateAllQuestions());
   const [set, setRight] = React.useState(false);
@@ -19,18 +21,21 @@ export default function Main() {
   }
 
   function chooseRandomAnswer() {
-    const arrAnswer = [];
+    const arrAnswer = []
     const categories = ["life", "business", "YesNot"];
     const randomNumber = Math.floor(Math.random() * categories.length);
     const randomNumber2 = Math.floor(Math.random() * dataArray.length);
     const choose = dataArray[randomNumber2].answers[categories[randomNumber]];
     const url = dataArray[randomNumber2].img;
-    arrAnswer.push(choose, url);
-    return arrAnswer;
+    arrAnswer.push(url, choose);
+    console.log(arrAnswer)
+    return arrAnswer
   }
+
   function generateAllQuestions() {
-    const url = chooseRandomAnswer()[1];
-    const newArray = { url: url, answerOptions: [] };
+    const a = chooseRandomAnswer()
+    console.log(a, a[0], a[1])
+    const newArray = { url: a[0], answerOptions: [] };
     for (let i = 0; i < 3; i++) {
       const newQuestion = {
         value: random(),
@@ -41,12 +46,11 @@ export default function Main() {
     }
 
     const rightAnswer = {
-      value: chooseRandomAnswer()[0],
+      value: a[1],
       isCorrect: true,
       id: 4,
     };
     newArray.answerOptions.push(rightAnswer);
-
     return [newArray];
   }
 
@@ -62,10 +66,16 @@ export default function Main() {
     } else {
       if (number > 0) {
         setNumber((prevNumber) => prevNumber - 1);
-      } else if (number === -1) {
+      } else if (number === 0) {
         setGame(true);
       }
     }
+  }
+
+  function refresh(){
+    setRight(false)
+    setGame(false)
+    setNumber(0)
   }
 
   console.log(quest);
@@ -81,7 +91,7 @@ export default function Main() {
           <div>
             <p className="whats-true">Ви програли</p>
             <div className="button-next">
-              <button onClick={generateQuestions} className="but">
+              <button onClick={refresh} className="but">
                 Почати знову
               </button>
             </div>
